a6c70c53366e187a9df1e21ec80cec756f05cc9a
ssiddiqa <siddiqasayma@gmail.com>
2024-12-29 11:45:55 +0600

    redo

--- mobile-frontend/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'], // For Expo projects
-    plugins: [
-      'react-native-reanimated/plugin', // Add the Reanimated plugin here
-    ],
-  };
-};
--- mobile-frontend/constants/api.ts
+++
@@ -1,2 +0,0 @@
-// constants/api.ts
-export const API_BASE_URL = 'http://localhost:5000/api';
